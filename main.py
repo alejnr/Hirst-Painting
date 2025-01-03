bf47443cc13233a593aea836568daf4982ebf6dd
Alex Menlah <alexamenlah@gmail.com>
2025-01-03 11:29:44 +0000

    extracting rgb colors from image

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import colorgram
 
 
-colors = colorgram.extract('image.jpg', 6)
+colors = colorgram.extract('image.jpg', 10)
 
-print(colors)
+list_of_colors = []
+for color in colors:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    new_color = (r, g, b)
+    list_of_colors.append(new_color)
+print(list_of_colors)
