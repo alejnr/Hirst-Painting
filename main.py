9fbb917f2d7f52a6bead4b3042166a25611c03b4
Alex Menlah <alexamenlah@gmail.com>
2025-01-03 12:09:38 +0000

    drawing the dots

--- main.py
+++ main.py
@@ -14,10 +14,28 @@ import random as rd
 
 tim = t.Turtle()
 t.colormode(255)
+tim.speed("fastest")
 
 list_of_colors = [(1, 9, 30), (121, 95, 41), (72, 32, 21), (238, 212, 72), (220, 81, 59), (226, 117, 100), (93, 1, 21), (178, 140, 170), (151, 92, 115), (35, 90, 26), (6, 154, 73), (205, 63, 91), (168, 129, 78), (3, 78, 28), (1, 64, 147), (221, 179, 218), (4, 220, 218), (80, 135, 179), (130, 157, 177), (81, 110, 135), (120, 187, 164), (11, 213, 220), (118, 18, 36), (243, 205, 7), (132, 223, 209), (229, 173, 165)]
 
-tim.dot(20, rd.choice(list_of_colors))
+tim.setheading(220)
+tim.penup()
+tim.forward(300)
+tim.setheading(0)
+tim.penup()
+
+num_of_dots = 100
+
+for dot_count in range(1, num_of_dots + 1):
+    tim.dot(20, rd.choice(list_of_colors))
+    tim.forward(50)
+
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
 
 
 screen = t.Screen()
